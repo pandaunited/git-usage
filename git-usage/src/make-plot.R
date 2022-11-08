0c8d6bf67d6f18795681a0a2ae9b13a6fc62df67
pandaunited <117743161+pandaunited@users.noreply.github.com>
2022-11-08 14:35:47 +0000

    Update make-plot.R

--- git-usage/src/make-plot.R
+++ git-usage/src/make-plot.R
@@ -38,7 +38,3 @@ ggsave(filename = "out/git-usage.png",
 sink(file = "out/regression-summary.txt")
 summary(lm(percentage ~ year, data = sods_data))
 sink()
-
-sink(file = "out/package-versions.txt")
-sessionInfo()
-sink()
